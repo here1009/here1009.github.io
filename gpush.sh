a17c528ab00f3922864cb236ac1b074974e2ea3d
here1009 <gao_yantao@yeah.net>
2017-03-09 17:22:27 +0800

    update

--- gpush.sh
+++ gpush.sh
@@ -1,7 +1,7 @@
 git add .
 timestamp=`date +%Y%m%d%r`
+echo $timestamp >> README.md;
 git commit -am "update"
-log=`git log | sed -n '1p' | sed  's/commit//'`
-echo $timestamp' - '$log >> README.md;
+#log=`git log | sed -n '1p' | sed  's/commit//'`
 
 git push
